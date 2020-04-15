888b46f1b331dbbd423ae6c1f363e19c7346885d
NguyenDuyBao <duybao.dragon@gmail.com>
2020-04-15 09:21:35 +0700

    change

--- cat.js
+++ cat.js
@@ -1,24 +1,10 @@
 function Cat(){
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	this.stomach = [];
 }
 
-cat.prototype.eat = function(mouse){
+Cat.prototype.eat = function(mouse){
  this.stomach.push(mouse);
 }
 
 module.exports = Cat;
-=======
-	
-}
->>>>>>> parent of 3782308... add stomach method
-=======
-	
-}
->>>>>>> parent of 3782308... add stomach method
-=======
-	
-}
->>>>>>> parent of 3782308... add stomach method
